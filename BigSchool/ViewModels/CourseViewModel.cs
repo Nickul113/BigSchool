2f73d83d04121f4adc08f1cc7b0cd3141c1bef8d
Quoc Cuong <nguyencuong200798@gmail.com>
2021-06-30 20:31:59 +0700

    Add project files.

---
+++ BigSchool/ViewModels/CourseViewModel.cs
@@ -0,0 +1,29 @@
+﻿using BigSchool.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool.ViewModels
+{
+    public class CourseViewModel
+    {
+        [Required]
+        public String Place { get; set; }
+        [Required]
+        [FutureDay]
+        public String Date { get; set; }
+        [Required]
+        [ValidTime]
+        public String Time { get; set; }
+        [Required]
+        public byte Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
+    }
+}
